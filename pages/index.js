45d8e07e0e90bc5dd6c8505444efd66521d17de5
Zhiyuan Zheng <me@xmflsct.com>
2021-04-20 22:59:00 +0200

    Demo

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import { useCallback, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import { Controlled } from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
+export default function Home () {
+  const [isZoomed, setIsZoomed] = useState(false)
+  const onZoomChange = useCallback(shouldZoom => {
+    setIsZoomed(shouldZoom)
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
         <p className={styles.description}>
@@ -20,18 +30,28 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.card} onClick={() => setIsZoomed(true)}>
+            <Controlled isZoomed={isZoomed} onZoomChange={onZoomChange}>
+              <Image
+                src='https://images.ctfassets.net/hb7jdg1fwzzk/IlK22NNIh778evBgxj7Ao/3b90eba7bb57cd11159a6a3f66ca722f/JK16-209-1_Cylinder_vase_M__h.16X12X9cm__stoneware__handpainted__TerraDelft.JPG?fm=jpg&fl=progressive&fit=fill&q=85&w=1080&h=1080'
+                layout='responsive'
+                width={270}
+                height={270}
+              />
+            </Controlled>
+          </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className={styles.card}>
+            <Image
+              src='https://images.ctfassets.net/hb7jdg1fwzzk/IlK22NNIh778evBgxj7Ao/3b90eba7bb57cd11159a6a3f66ca722f/JK16-209-1_Cylinder_vase_M__h.16X12X9cm__stoneware__handpainted__TerraDelft.JPG?fm=jpg&fl=progressive&fit=fill&q=85&w=1080&h=1080'
+              layout='responsive'
+              width={270}
+              height={270}
+            />
+          </div>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href='https://github.com/vercel/next.js/tree/master/examples'
             className={styles.card}
           >
             <h3>Examples &rarr;</h3>
@@ -39,7 +59,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
             className={styles.card}
           >
             <h3>Deploy &rarr;</h3>
@@ -52,12 +72,12 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
         </a>
       </footer>
     </div>
